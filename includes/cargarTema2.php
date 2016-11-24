bb8df294eff079f302d40b0276f29a528b5864c0
MartoGonzalvo <gonzalvomartin@gmail.com>
2016-11-24 00:44:13 -0300

    subir archivos

--- includes/cargarTema2.php
+++ includes/cargarTema2.php
@@ -1,11 +1,13 @@
 <?php
+	ini_set('post_max_size','100M');
 	$carpeta="../mp3/";
 	opendir($carpeta);
 	$destino=$carpeta.$_FILES['foto']['name'];
-	copy($_FILES['foto']['tmp_name'],$destino);
+	
+	move_uploaded_file($_FILES['foto']['tmp_name'],$destino);
 	echo "subido<br>";
 	$nombre=$_FILES['foto']['name'];
-	echo "<img src=\"../mp3/$nombre\"><br>";
-	echo $_FILES['foto']['nombre'];
+	//echo "<img src=\"../mp3/$nombre\"><br>";
+	echo $_FILES['foto']['name'] ;
 	echo $_FILES['foto']['error'];
 	?>
